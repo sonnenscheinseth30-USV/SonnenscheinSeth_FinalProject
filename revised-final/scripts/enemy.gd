6d06731bcaa7fc7cf474afbabb5bfd9416b101ea
sonnenscheinseth30-USV <sonnenscheinseth30@gmail.com>
2026-04-12 18:55:56 -0700

    Final (set dressing)

--- revised-final/scripts/enemy.gd
+++ revised-final/scripts/enemy.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var enemykill: AudioStreamPlayer2D = $enemykill
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+		
 
 
 func _on_hit_area_body_entered(body: Node2D) -> void:
 	if "player" in body.name:
-		queue_free()
+		queue_free() # Replace with function body.
--- revised-final/scripts/player.gd
+++ revised-final/scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var player_jump: AudioStreamPlayer2D = $PlayerJump
+
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		player_jump.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
